b95ca35373cff5c8f57f5d6bde1d1cc5264d01a9
mejiabrayan <learnwithbrayan@gmail.com>
2022-12-14 05:39:21 -0800

    included comments

--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -3,7 +3,25 @@ import { UserLoginFormComponent } from '../user-login-form/user-login-form.compo
 import { UserRegistrationFormComponent } from '../user-registration-form/user-registration-form.component';
 import { MatDialog } from '@angular/material/dialog';
 
+/**
+ * @component WelcomePageComponent
+ * @description This component is the welcome page of the application.
+ * @class WelcomePageComponent
+ * @implements OnInit
+ * @property {MatDialog} dialog - This property is used to open the dialog box.
+ * @method openUserRegistrationDialog - This method is used to open the user registration dialog box.
+ */
+
+
 @Component({
+
+  /**
+   * @property {string} selector - This property is used to define the selector of the component.
+   * @property {string} templateUrl - This property is used to define the template of the component.
+   * @property {string} styleUrls - This property is used to define the style of the component.
+   */
+
+
   selector: 'app-welcome-page',
   templateUrl: './welcome-page.component.html',
   styleUrls: ['./welcome-page.component.scss'],
@@ -11,11 +29,26 @@ import { MatDialog } from '@angular/material/dialog';
 export class WelcomePageComponent implements OnInit {
   constructor(public dialog: MatDialog) {}
   ngOnInit(): void {}
+
+
+  /**
+   * @method openUserRegistrationDialog
+   * @description This method is used to open the user registration dialog box.
+   * @returns {void}
+   */
+
+
   openUserRegistrationDialog(): void {
     this.dialog.open(UserRegistrationFormComponent, {});
   }
+  /**
+   * @method openUserLoginDialog
+   * @description This method is used to open the user login dialog box.
+   * @returns {void}
+   */
+
+
   openUserLoginDialog(): void {
     this.dialog.open(UserLoginFormComponent, {});
   }
-  
 }
